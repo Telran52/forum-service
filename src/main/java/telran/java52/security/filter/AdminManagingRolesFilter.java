080245abe5d802379289408875621b73d43df76b
edward <edwardlfish@gmail.com>
2024-05-20 21:15:14 +0300

    AuthenticationFilter

---
+++ src/main/java/telran/java52/security/filter/AdminManagingRolesFilter.java
@@ -0,0 +1,30 @@
+package telran.java52.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(20)
+public class AdminManagingRolesFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		// TODO Auto-generated method stub
+		
+		chain.doFilter(request, response);
+	}
+
+}
